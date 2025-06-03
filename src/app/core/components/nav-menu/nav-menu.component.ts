04e385d1d24300e1aeb06fd89e4c0b2cc6747943
leninner <mazabandalenin180@gmail.com>
2025-06-03 15:40:58 -0500

    chore: fix

--- src/app/core/components/nav-menu/nav-menu.component.ts
+++ src/app/core/components/nav-menu/nav-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   standalone: true,
-  imports: [IonicModule, RouterModule]
+  imports: [IonicModule, RouterModule, CommonModule]
 })
 export class NavMenuComponent {} 
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, RouteReuseStrategy, withPreloading, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app/app-routing.module';
+import { addIcons } from 'ionicons';
+import { add, create, trash, book, people, list, home } from 'ionicons/icons';
 
 import { AppComponent } from './app/app.component';
+
+addIcons({ add, create, trash, book, people, list, home });
 
 bootstrapApplication(AppComponent, {
   providers: [
